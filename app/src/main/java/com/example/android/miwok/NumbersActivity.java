f42552fd1f75260b28a8dba671ab28238b0a66d6
Jani Raiski <jani.raiski@gmail.com>
2018-03-13 23:06:39 +0200

    Interface and functionality changes

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -3,32 +3,49 @@ package com.example.android.miwok;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
-public class NumbersActivity extends AppCompatActivity {
+public class NumbersActivity extends AppCompatActivity
+{
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_numbers);
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_numbers);
 
-        ArrayList<String> words = new ArrayList<String>();
-        words.add(0, "one");
-        words.add(1, "two");
-        words.add(2, "three");
-        words.add(3, "four");
-        words.add(4,"five");
-        words.add(5, "six");
-        words.add(6, "seven");
-        words.add(7, "eight");
-        words.add(8, "nine");
-        words.add(9, "ten");
+		ArrayList<String> words = new ArrayList<String>();
+		words.add(0, "one");
+		words.add(1, "two");
+		words.add(2, "three");
+		words.add(3, "four");
+		words.add(4, "five");
+		words.add(5, "six");
+		words.add(6, "seven");
+		words.add(7, "eight");
+		words.add(8, "nine");
+		words.add(9, "ten");
 
-        for (int i = 0; i < words.size(); i++){
-            Log.v("NumbersActivity", "Word at index[" + i + "]: " + words.get(i));
-        }
+		// Makes a variable from the activity_numbers.xml root view
+		LinearLayout rootView = findViewById(R.id.rootView);
 
 
-    }
+		int index = 0;
+
+		while (index < words.size())
+		{
+			TextView wordView = new TextView(this);
+			wordView.setText(words.get(index));
+
+			// Adding wordView as a child of rootView
+			rootView.addView(wordView);
+
+			index++;
+		}
+	}
 }
